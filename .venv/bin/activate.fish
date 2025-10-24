46d8be9cca410c9a7c886b1762cf7d0982fd1205
code-j <1424774373@qq.com>
2025-10-24 23:21:28 +0800

    ROUTINE

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/code-j/data/CS/python/.venv
+set -gx VIRTUAL_ENV /home/code-j/data/CS/python3/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
